e9b5385aff5c484c4dd59368ce4f8f57fe9c6804
SnappiBrawn <creativeswaroop@gmail.com>
2024-05-17 22:35:04 +0530

    home page first draft

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,66 @@
+// scroll top visibility function
+$(document).ready(function () {
+  $(window).scroll(function () {
+    var scrollHeight = window.scrollY;
+    var windowHeight = $(window).height();
 
-$(document).ready(function() {
-    $(window).scroll(function() {
-      var scrollHeight = window.scrollY; // Get document scroll height
-      var windowHeight = $(window).height(); // Get viewport height
-  
-      // Check if scroll height is greater than 100vh
-      if (scrollHeight > windowHeight) {
-        $("#scroll-top").css("opacity", 100); // Set display to flex
-      } else {
-        $("#scroll-top").css("opacity", 0); // Reset display property
-      }
-    });
+    if (scrollHeight > windowHeight) {
+      $("#scroll-top").css("opacity", 100);
+    } else {
+      $("#scroll-top").css("opacity", 0);
+    }
   });
+});
 
+var owl = $('.owl-carousel.reviews-carousel').owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: false,
+  autoplay: true,
+  autoplayTimeout: 4000,
+  responsive: {
+    0: {
+      items: 1
+    }
+  }
+})
 
+$('#reviews-next').click(function () {
+  owl.trigger('next.owl.carousel');
+})
+
+$('#reviews-prev').click(function () {
+  owl.trigger('prev.owl.carousel');
+})
+
+$('.owl-carousel.facts-carousel').owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  autoplay: true,
+  autoplayTimeout: 4000,
+  responsive: {
+    0: {
+      items: 1
+    },
+    1000: {
+      items: 6
+    },
+  }
+})
+
+$('.owl-carousel').owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  autoplay: true,
+  autoplayTimeout: 4000,
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 3
+    },
+  }
+})
